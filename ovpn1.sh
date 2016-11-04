9d33919ece807011ee41b8984a9a96c5ad667b2b
nami1707 <sairmansairman@gmail.com>
2016-11-04 19:20:24 +0800

    Update ovpn1.sh

--- ovpn1.sh
+++ ovpn1.sh
@@ -2,13 +2,14 @@ apt-get update
 apt-get upgrade -y
 apt-get install nano -y
 apt-get install curl -y
+apt-get install openvpn -y
 echo "/bin/false" >> /etc/shells
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.690_all.deb && dpkg -i webmin_1.690_all.deb
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 apt-get -f install -y
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
-apt-get install -y openvpn
+apt-get install openvpn -y
 cp -a /usr/share/doc/openvpn/examples/easy-rsa /etc/openvpn/
 cd /etc/openvpn/easy-rsa/2.0
 source ./vars
